96015d2c75163531ab58ec403824d93ec5d753a1
ryzeN001 <104761581+ryzeN001@users.noreply.github.com>
2022-06-22 11:22:08 +0800

    Update ping_interval.py

--- ping_interval.py
+++ ping_interval.py
@@ -5,7 +5,7 @@ import psycopg2
 from datetime import datetime
 import pytz
 
-conn = psycopg2.connect(database="covid-nineteen", user="postgres", password="admin", host="localhost", port="5432")
+conn = psycopg2.connect(database="demo", user="postgres", password="admin", host="localhost", port="5432")
 
 cur = conn.cursor()
 cur.execute("SELECT  daily_logout_time from dashboard_eventday")
@@ -21,7 +21,7 @@ for row in rows:
     tz_Manila = pytz.timezone('Asia/Manila')
     datetime_Manila = datetime.now(tz_Manila)
     current_time = datetime_Manila.strftime("%H:%M:%S")
-    timeout_status = ''
+ 
 
     if reachable == 0:
         cur.execute(f"UPDATE user_user set status = 'Online', present = 'True' where user_idnumber = '{row[0]}'")
@@ -30,7 +30,7 @@ for row in rows:
         cur.execute(f"UPDATE user_user set timeout = '{current_time}', status = 'Offline', present = 'False' where user_idnumber = '{row[0]}'")
         conn.commit()
     elif reachable != 0 and row[7] != None and row[6] == 'Online':
-        cur.execute(f"UPDATE user_user set timeout = '{current_time}', timeout_status = '{timeout_status}', prev_timeout = '{row[7]}', status = 'Offline', present = 'False' where user_idnumber = '{row[0]}'")
+        cur.execute(f"UPDATE user_user set timeout = '{current_time}', prev_timeout = '{row[7]}', status = 'Offline', present = 'False' where user_idnumber = '{row[0]}'")
         conn.commit()
     elif reachable != 0 and prevStatus == 'Offline':
         cur.execute(f"UPDATE user_user set status = 'Offline' where user_idnumber = '{row[0]}'")
